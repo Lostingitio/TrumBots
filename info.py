cde968944bbfb1b32215efdff437c8b36c5c576c
LieStage <118051769+LieStage@users.noreply.github.com>
2023-06-24 19:38:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -70,5 +70,5 @@ LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies 
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
-SHORTENER_API = environ.get("SHORTENER_API", "598224881ee80c102a0c28012f77a0afade7921d")
-LONG_DROPLINK_URL = environ.get("LONG_DROPLINK_URL","https://urlshorten.in/api")
+SHORTENER_API = environ.get("SHORTENER_API", "0406398c8bdbd58e938529b94cd1146c95f6b666")
+LONG_DROPLINK_URL = environ.get("LONG_DROPLINK_URL","https://cyberurl.me/api")
