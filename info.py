9670dd207c57fe936484f812f3ab516168f40cf0
LieStage <118051769+LieStage@users.noreply.github.com>
2023-08-06 08:05:06 +0530

    Update info.py
    Mdisklink

--- info.py
+++ info.py
@@ -70,5 +70,5 @@ LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies 
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
-SHORTENER_API = environ.get("SHORTENER_API", "e3ccec0e917477aeefc9675fb2d2701c140e56dd")
-LONG_DROPLINK_URL = environ.get("LONG_DROPLINK_URL","https://dalink.in/api")
+SHORTENER_API = environ.get("SHORTENER_API", "299fd5c74bbe61161f2ef8a6f11c947dbc8d53a9")
+LONG_DROPLINK_URL = environ.get("LONG_DROPLINK_URL","https://mdisklink.link/api")
